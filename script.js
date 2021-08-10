9c508b0c65c8b47cf1b68e203163c4bb7ddfbc90
jinyangb <your_email@example.com>
2021-08-10 11:09:17 -0400

    added setTimeout

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let simon = []
 let turns = 0
 let player = []
-const startGame = document.getElementById('start')
+const startGame = document.querySelector('.start')
+let clicks = document.querySelectorAll('.box')
+
+clicks.addEventListener('click', function () {})
 
 function simonGoes() {
   simon.push(Math.floor(4 * Math.random()) + 1)
@@ -10,6 +13,19 @@ function simonGoes() {
     player.push(clicks)
   }
 }
+function activateBox(color) {
+  const box = document.querySelectorAll(`[data-color='${color}']`)
+  setTimeout(() => {
+    box.classList.remove('activated')
+  }, 300)
+}
+// if (simon === player) {
+//   turns += 1
+// } else {
+//   turns = 0
+// }
+// simonGoes()
+// console.log(simonGoes)
 // if (simon === player) {
 //   function simonGoes() {
 //     turns += 1

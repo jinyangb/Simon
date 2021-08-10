9cd441e19fae6b75251b70ff7fe807ea4c88f27b
jinyangb <your_email@example.com>
2021-08-10 13:12:29 -0400

    simon array works

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 let simon = []
 let turns = 0
 let player = []
+let boxes = document.querySelectorAll('.box')
 const startGame = document.querySelector('.start')
-let clicks = document.querySelectorAll('.box')
-
-clicks.addEventListener('click', function () {})
+let clicks = document.querySelector('.gameboard')
 
 function simonGoes() {
-  simon.push(Math.floor(4 * Math.random()) + 1)
-  turns += 1
-  function playerGoes() {
-    player.push(clicks)
-  }
+  let tile = boxes[Math.floor(Math.random() * boxes.length)]
+  let id = parseInt(tile.dataset.id)
+  simon.push(id)
 }
-function activateBox(color) {
-  const box = document.querySelectorAll(`[data-color='${color}']`)
-  setTimeout(() => {
-    box.classList.remove('activated')
-  }, 300)
-}
-// if (simon === player) {
-//   turns += 1
-// } else {
-//   turns = 0
+simonGoes()
+console.log(simon)
+boxes.forEach((box) => {
+  box.addEventListener('click', (event) => {
+    let id = parseInt(event.target.dataset.id)
+    player.push(id)
+    console.log(player)
+    simonGoes()
+    console.log('simon', simon)
+  })
+})
+
+// function activateBox(color) {
+//   const box = document.querySelector(`[data-color='${color}']`)
+//   setTimeout(() => {
+//     box.classList.remove('activated')
+//   }, 300)
 // }
-// simonGoes()
-// console.log(simonGoes)
-// if (simon === player) {
-//   function simonGoes() {
-//     turns += 1
-//   }
-// } else {
-//   turns = 0
-// }
+
+// click begin
+// simon goes once
+// simon array get one random value between 1 and 4
+// player goes once
+// player array gets a value based on box pressed
+// compare arrays to check for success
+// if successful simons goes
+// if unsuccessful restart

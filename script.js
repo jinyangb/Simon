3e0434b5ffc0bd65738bff03f226e81265aefbdc
jinyangb <your_email@example.com>
2021-08-10 20:57:20 -0400

    added begin button

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let simon = []
-let turns = 0
+let count = 0
 let player = []
+const begin = document.querySelector('#start')
 let boxes = document.querySelectorAll('.box')
-const startGame = document.querySelector('.start')
+// const startGame = document.querySelector('.start')
 let clicks = document.querySelector('.gameboard')
 
+console.log(begin)
+begin.addEventListener('click', () => {
+  simonGoes()
+})
 function simonGoes() {
   let tile = boxes[Math.floor(Math.random() * boxes.length)]
   let id = parseInt(tile.dataset.id)
@@ -12,22 +17,24 @@ function simonGoes() {
 }
 simonGoes()
 console.log(simon)
-boxes.forEach((box) => {
+boxes.forEach((box, i) => {
   box.addEventListener('click', (event) => {
     let id = parseInt(event.target.dataset.id)
     player.push(id)
     console.log(player)
     simonGoes()
     console.log('simon', simon)
+    result(i)
   })
 })
 
-// function activateBox(color) {
-//   const box = document.querySelector(`[data-color='${color}']`)
-//   setTimeout(() => {
-//     box.classList.remove('activated')
-//   }, 300)
-// }
+function result(i) {
+  if (simon[i] === player[i]) {
+    console.log('simon = player[i]')
+  } else {
+    alert('Game Over')
+  }
+}
 
 // click begin
 // simon goes once
